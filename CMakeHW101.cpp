a741c7a5bc1f609522decc5339ecc897466825d6
WhiteMasterYuraCPP-20 <yuravolchkov@gmail.com>
2024-07-11 20:56:01 +0300

    first Commit

---
+++ CMakeHW101.cpp
@@ -0,0 +1,15 @@
+﻿#include "CMakeHW101.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // задать русский текст
+    system("chcp 1251"); // настроить кодировку консоли
+    system("cls"); // очистить экран
+    string Name_Enter;
+    cout << "Введите имя: ";
+    cin >> Name_Enter;
+    cout << "\nЗдравствуйте," << Name_Enter << "!" << endl;
+
+    return 101;
+
+}
